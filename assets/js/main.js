835acc60ff026f00035a5990d40b9f75adb2d7d2
karinlee <karinlee92@gmail.com>
2023-01-03 12:17:52 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+document.write(b);
+
+console.log(document.URL);
+
+console.log("Hello World!");
+console.log("Anton");
+
+let firstName="Anton";
+console.log(firstName);
+
+let age= 28;
+console.log(age);
+
+let job ="Trainer"
+console.log(job);
+
+married = (true);
+console.log(married);
+
+console.log(firstName + " ist " + age + " Jahre alt und ist verheiratet: " + married);
